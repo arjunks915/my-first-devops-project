f5814b79e65b44a97e578464864c6c17ed8ad5cd
arjunks915 <arjunshambu915@gmail.com>
2026-06-28 13:57:32 +0000

    Added automation script

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print("Hello, arjun! Your DevOps journey has officially started.")
+import os
+
+# Create a new folder automatically
+os.makedirs("devops_folder", exist_ok=True)
+
+# Create a text file inside that folder and write into it
+with open("devops_folder/status.txt", "w") as file:
+    file.write("Automation works! This file was created by Python.")
+
+print("Folder and file created successfully via code!")
